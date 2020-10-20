107e79618588cbf1b8068d2515e95abf5111c687
Gautier Boder <gautier.boder@garaio.com>
2020-10-20 08:41:00 +0200

    trying to fix gateway container

--- test/Demo/src/Gbo.Ocelot.Tests/Build-Container.ps1
+++ test/Demo/src/Gbo.Ocelot.Tests/Build-Container.ps1
@@ -1,3 +1,3 @@
 dotnet clean
-dotnet publish -c release --self-contained -r linux-x64
-docker build -t consul-demo-gw .\bin\release\netcoreapp3.1\linux-x64\publish\
+dotnet publish -c debug --self-contained -r linux-x64
+docker build -t consul-demo-gw .\bin\debug\netcoreapp3.1\linux-x64\publish\
