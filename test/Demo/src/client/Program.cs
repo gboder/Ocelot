09cff039844a42bbcd83398ddc7c217ff7be2fa8
Gautier Boder <gautier.boder@garaio.com>
2020-11-20 08:58:02 +0100

    working on client and refactor consul config

---
+++ test/Demo/src/client/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace client
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+
+        private static async GetNodeInfo(string url)
+        {
+            using (var httpClient = new HttpClient())
+            {
+                var 
+            }
+
+        }
+    }
+
+    class NodeInfo
+    {
+        public string HOSTNAME { get; set; }
+        public string APP_SERVICE_NAME { get; set; }
+    }
+}
